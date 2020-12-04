ac844c5ba56a4c99a3cff47b2b5d6744bcd54b62
Jorge Bazaga <jose.junior@lifecom.com.br>
2020-12-03 22:54:05 -0300

    Alterando font apra Roboto

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -4,7 +4,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      App
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Non repellat commodi quod vel velit distinctio, repellendus sapiente alias quaerat, cumque beatae incidunt totam officiis, porro earum. Obcaecati alias veritatis facere.</p>
+      <b>adada</b>
     </div>
   );
 }
